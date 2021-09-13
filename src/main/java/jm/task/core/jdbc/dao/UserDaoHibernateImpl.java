33c9a67e707567b857a6e1b4c0da92342ccd2e4e
Shamil <super.shomka@mail.ru>
2021-09-14 01:03:50 +0300

    Я переделал

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.*;
 
@@ -13,8 +14,7 @@ import java.util.ListIterator;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-        SessionFactory sessionFactory = Util.getSessionFactory();
-        Session session = null;
+    Session session = null;
 
     public UserDaoHibernateImpl() {
 
@@ -23,106 +23,59 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try {
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            Query query = session.createSQLQuery("CREATE TABLE `mydbtest`.`UserDao` (\n" +
-                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                    "  `name` VARCHAR(45) NOT NULL,\n" +
-                    "  `lastname` VARCHAR(45) NOT NULL,\n" +
-                    "  `age` INT(3) NOT NULL,\n" +
-                    "  PRIMARY KEY (`id`))").addEntity(User.class);
-            query.executeUpdate();
-        } catch (NullPointerException e) {
-            System.out.println("Проверьте подключение к БД");
-        } catch (Exception e) {
-            System.out.println("Такая таблица уже создана");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        Query query = session.createSQLQuery("CREATE TABLE `mydbtest`.`UserDao` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `lastname` VARCHAR(45) NOT NULL,\n" +
+                "  `age` INT(3) NOT NULL,\n" +
+                "  PRIMARY KEY (`id`))").addEntity(User.class);
+        query.executeUpdate();
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            Query query = session.createSQLQuery("drop  table `mydbtest`.`UserDao`").addEntity(User.class);
-            query.executeUpdate();
-        } catch (NullPointerException e) {
-            System.out.println("Проверьте подключение к БД");
-        } catch(Exception e) {
-            System.out.println("Такой таблицы не существует");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        Query query = session.createSQLQuery("drop  table `mydbtest`.`UserDao`").addEntity(User.class);
+        query.executeUpdate();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            session = sessionFactory.getCurrentSession();
-            User user = new User();
-            user.setName(name);
-            user.setLastName(lastName);
-            user.setAge(age);
-            session.beginTransaction();
-            session.save(user);
-            // commit() - следовательно сессию закрыли
-            System.out.println("Пользователь " + name + " Добавлен в таблицу");
-        } catch (HibernateException e) {
-            System.out.println(e);
-        } catch (NullPointerException e) {
-            System.out.println("Не удалось установить соединение с БД");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        User user = new User();
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setAge(age);
+        session.save(user);
+        // commit() - следовательно сессию закрыли
+        System.out.println("Пользователь " + name + " Добавлен в таблицу");
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            User userFromDB = (User) session.get(User.class, id);
-            session.delete(userFromDB);
-        } catch (NullPointerException exception) {
-            System.out.println("Проверьте подключение к БД");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        User userFromDB = (User) session.get(User.class, id);
+        session.delete(userFromDB);
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        try{
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            userList = session.createCriteria(User.class).list();
-        } catch (NullPointerException throwables) {
-            System.out.println("Проверьте подключение к БД");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        List<User> userList = session.createCriteria(User.class).list();
         return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-        try{
-            session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
-            List<User> userList = session.createCriteria(User.class).list();
-            ListIterator<User> listIterator = userList.listIterator();
-            while(listIterator.hasNext()){
-                session.delete(userList.get(listIterator.nextIndex()));
-                listIterator.next();
-            }
-        } catch (NullPointerException exception) {
-            System.out.println("Проверьте подключение к БД");
-        } finally {
-            session.getTransaction().commit();
-        }
+        session = UserServiceImpl.getSession();
+        session.beginTransaction();
+        session.createSQLQuery("delete from userdao").addEntity(User.class).executeUpdate();
     }
+
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,35 +3,88 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDao userDao = new UserDaoHibernateImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+    private static Session session = null;
 
-    public void createUsersTable() throws SQLException {
-        userDao.createUsersTable();
+
+    public void createUsersTable() {
+        try {
+            session = sessionFactory.getCurrentSession();
+            userDao.createUsersTable();
+        } catch (Exception e) {
+            System.out.println("Таблица уже создана");
+        } finally {
+            session.getTransaction().commit();
+        }
     }
 
-    public void dropUsersTable() throws SQLException {
-        userDao.dropUsersTable();
+    public void dropUsersTable() {
+        try {
+            session = sessionFactory.getCurrentSession();
+            userDao.dropUsersTable();
+        } catch (Exception e) {
+            System.out.println("Таблицы не существует");
+        } finally {
+            session.getTransaction().commit();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDao.saveUser(name, lastName, age);
+        try {
+            session = sessionFactory.getCurrentSession();
+            userDao.saveUser(name, lastName, age);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            session.getTransaction().commit();
+        }
     }
 
     public void removeUserById(long id) throws SQLException {
-        userDao.removeUserById(id);
+        try {
+            session = sessionFactory.getCurrentSession();
+            userDao.removeUserById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.getTransaction().commit();
+        }
     }
 
     public List<User> getAllUsers() throws SQLException {
-        return userDao.getAllUsers();
+        try {
+            session = sessionFactory.getCurrentSession();
+            return userDao.getAllUsers();
+        } finally {
+            session.getTransaction().commit();
+        }
     }
 
     public void cleanUsersTable() throws SQLException {
-        userDao.cleanUsersTable();
+        try {
+            session = sessionFactory.getCurrentSession();
+            userDao.cleanUsersTable();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.getTransaction().commit();
+        }
     }
+
+    public static Session getSession() {
+        return session;
+    }
+
 }
+
+
